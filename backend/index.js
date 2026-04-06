e9f71d0ca21bbd1b1c224ba696d80653c56f5c96
tronflynn2000 <tronflynn2000@gmail.com>
2026-04-06 13:01:02 +0200

    Basic structure of the backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+//Here I'll initiate a server in order to build the backend of this neobank
+//app of mine.
+import express from "express";
+
+const app = express();
+const puerto = 3000;
+app.get("/about", (req,res)=> {
+    res.send("<h1>About Me</h1>");
+});
+
+app.get("/", (req, res) =>{
+    res.send("<h1> Hello World<h1/>");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port ", puerto);
+})
